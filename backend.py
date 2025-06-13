dee89cbb70144a1c91d8072e44950c125d6dc351
Manish kumar yadav <120595460+manish6299@users.noreply.github.com>
2025-06-14 00:15:43 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -55,4 +55,5 @@ def chat_endpoint(request: RequestState):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
